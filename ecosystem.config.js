7bfa6b55d3250c0d69df6c70e8e644e23d5b90d8
Ubuntu <ubuntu@ip-172-31-20-25.us-east-2.compute.internal>
2025-05-30 11:13:52 +0000

    Remove default example

---
+++ ecosystem.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  apps: [
+    {
+      name: 'react-boilerplate',
+      script: 'npm',
+      args: 'start',
+      cwd: '/var/www2',
+      env: {
+        NODE_ENV: 'development',
+        NODE_OPTIONS: '--openssl-legacy-provider',
+      },
+      env_production: {
+        NODE_ENV: 'production',
+        NODE_OPTIONS: '--openssl-legacy-provider',
+      },
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      error_file: './logs/err.log',
+      out_file: './logs/out.log',
+      log_file: './logs/combined.log',
+      time: true,
+    },
+  ],
+};
